b8fddfb476362c317f34375b7243f7c592880aa2
Andreas Lochwitz <andreas_lochwitz@live.de>
2017-02-08 21:12:37 +0100

    First draft
    Added styling of results

---
+++ js/script.js
@@ -0,0 +1,52 @@
+document.getElementById("logButton").addEventListener("click", function() {
+  var moment = new Date(),
+      resultDiv = null;
+  console.log(getDate(moment));
+  console.log(getTime(moment));
+  console.log(getTimeStamp(moment));
+  //alert("klick - " + getTime(moment) + " - " + getTimeStamp(moment));
+  resultDiv = document.createElement("div");
+  resultDiv.innerHTML = getTime(moment) + "<br /><b>" + getTimeStamp(moment) + "</b>";
+  resultDiv.className = "resultEntry";
+  var resultsDiv = document.getElementById("logResults");
+  if (resultsDiv.firstChild) {
+    resultsDiv.insertBefore(resultDiv, resultsDiv.firstChild);
+  } else {
+    resultsDiv.appendChild(resultDiv);
+  }
+});
+
+
+function prefix(digit) {
+  var prefix = "";
+  if (digit < 10) prefix = "0";
+  return prefix + digit;
+}
+
+function getTimeStamp(moment) {
+  return moment.valueOf();
+}
+
+function getDate(moment) {
+  var year = moment.getFullYear(),
+      month = prefixString(moment.getMonth() + 1, 2),
+      day = prefixString(moment.getDate(), 2);
+  return year + "-" + month + "-" + day;
+}
+
+function getTime(moment) {
+  var hours = prefixString(moment.getHours(), 2),
+      minutes = prefixString(moment.getMinutes(), 2),
+      seconds = prefixString(moment.getSeconds(), 2),
+      mSeconds = prefixString(moment.getMilliseconds(), 3);
+  return hours + ":" + minutes + ":" + seconds + "." + mSeconds;
+}
+
+function prefixString(number, digits, prefixChar) {
+  var result = "" + number;
+  prefixChar = (!prefixChar) ? "0": prefixChar;
+  while (result.length < digits) {
+    result = prefixChar + result;
+  }
+  return result;
+}
